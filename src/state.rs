708448f18be2088157f295d6e4a29e087e2b8e50
David Rodriguez <81386118+David-Sod@users.noreply.github.com>
2023-03-30 15:13:56 -0700

    fix(SHD-34): Adjust panic_withdraw to withdraw only panic unbond’s ids (#29)

--- src/state.rs
+++ src/state.rs
@@ -2,11 +2,12 @@ use cosmwasm_std::{Addr, StdResult, Storage};
 use secret_toolkit::storage::Item;
 use secret_toolkit::{serialization::Json, storage::Keymap};
 
-use crate::msg::{Config, ContractStatusLevel, InProcessUnbonding};
+use crate::msg::{Config, ContractStatusLevel, InProcessUnbonding, PanicUnbond};
 use crate::staking_interface::Unbonding;
 
 pub const KEY_CONFIG: &[u8] = b"config";
 pub const KEY_STAKING_INFO: &[u8] = b"staking_info";
+pub const KEY_PANIC_UNBONDS: &[u8] = b"panic_unbonds_ids";
 pub const KEY_PENDING_UNBONDING: &[u8] = b"last_unbonding_id";
 pub const KEY_CONTRACT_STATUS: &[u8] = b"contract_status";
 pub const PREFIX_REVOKED_PERMITS: &str = "revoked_permits";
@@ -15,9 +16,11 @@ pub const PREFIX_UNBONDINGS: &[u8] = b"unbondings";
 pub const RESPONSE_BLOCK_SIZE: usize = 256;
 pub const UNBOND_REPLY_ID: u64 = 1_u64;
 pub const PANIC_WITHDRAW_REPLY_ID: u64 = 2_u64;
+pub const PANIC_UNBOND_REPLY_ID: u64 = 3_u64;
 
 pub static CONTRACT_STATUS: Item<ContractStatusLevel, Json> = Item::new(KEY_CONTRACT_STATUS);
 pub static CONFIG: Item<Config> = Item::new(KEY_CONFIG);
+pub static PANIC_UNBONDS: Item<Vec<PanicUnbond>> = Item::new(KEY_PANIC_UNBONDS);
 pub static PENDING_UNBONDING: Item<InProcessUnbonding> = Item::new(KEY_PENDING_UNBONDING);
 pub static UNBONDINGS_IDS: Item<Vec<u128>> = Item::new(PREFIX_UNBONDINGS_IDS);
 pub static UNBONDING: Keymap<u128, Unbonding> = Keymap::new(PREFIX_UNBONDINGS);
